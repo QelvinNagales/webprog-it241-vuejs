4c3aeef211699969b83532ab57de1b0e0533a3b3
Qelvin Joszeler D. Nagales <qdnagales@student.apc.edu.ph>
2026-02-07 23:55:55 +0800

    added rest api

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,15 +1,19 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/RestApi.vue'
 
 
 const app = createApp(App)
+app.component('personal-profile', PersonalProfile)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
+app.component('rest-api', RestApi)
 app.mount('#app')
